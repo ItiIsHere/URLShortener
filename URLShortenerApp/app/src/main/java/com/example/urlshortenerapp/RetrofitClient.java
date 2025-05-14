b5d3b3139442a165d054adc9e40e7b28e282cc04
Eliasib González <itiel.gon@gmail.com>
2025-05-13 20:31:13 -0600

    modificiación a server,js

--- URLShortenerApp/app/src/main/java/com/example/urlshortenerapp/RetrofitClient.java
+++ URLShortenerApp/app/src/main/java/com/example/urlshortenerapp/RetrofitClient.java
@@ -4,12 +4,12 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static Retrofit retrofit;
+    private static Retrofit retrofit = null;
 
     public static Retrofit getInstance() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl("http://10.0.2.2:5000/")
+                    .baseUrl("https://urlshortener-production-3cf7.up.railway.app/")
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
